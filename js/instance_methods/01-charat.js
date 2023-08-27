781fd302b3c8318cf4fe90edbcf7e57f371c1702
Mehedi Hasan <hellomehediwd@gmail.com>
2023-08-27 11:20:00 +0600

    Add charAt() short documentation

---
+++ js/instance_methods/01-charat.js
@@ -0,0 +1,25 @@
+/**
+ * Syntax: charAt(index)
+ */
+
+/**
+ * Return value: A string representing the character (exactly one UTF-16 code unit) at the specified index. If index is out of the range of 0 – str.length - 1, charAt() returns an empty string.
+ */
+
+const greeting = "AS-Salamu 'Alaikum";
+
+console.log(`The character at index ${7} is ${greeting.charAt(7)}`);
+// -> 'm'
+
+console.log(`The character at index ${-1} is ${greeting.charAt(-1)}`);
+// -> ''
+
+console.log(`The character at index ${100} is ${greeting.charAt(100)}`);
+// -> ''
+
+console.log(
+  `The character at index ${greeting.length - 2} is ${greeting.charAt(
+    greeting.length - 2
+  )}`
+);
+// -> 'u'
